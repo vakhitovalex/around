7edab4660effcd266e0a3ff647d4a221df84a78d
Alex Vakhitov <alexander.vakhitov@appfolio.com>
2020-09-06 20:57:30 -0700

    middle work

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const modalEditButton = document.querySelector('.profile__edit');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close-button');
+// Modals
+const editProfileModal = document.querySelector('.modal_type_edit-profile');
+const newPlaceModal = document.querySelector('.modal_type_add-place');
+// Open Modals buttons
+const profileEditButton = document.querySelector('.profile__edit');
+const addPlaceModal = document.querySelector('.profile__add');
+//Close Buttons
+const closeEditProfileModal = editProfileModal.querySelector('.modal__close-button');
+const closeNewPlaceModal = newPlaceModal.querySelector('.modal__close-button')
+// profile
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
+//form inputs
 const form = document.querySelector('.form');
-const formName = document.querySelector('.form__name');
-const formAbout = document.querySelector('.form__about');
+const formName = document.querySelector('.form__input_type_profile-name');
+const formAbout = document.querySelector('.form__input_type_profile-description');
+
+addPlaceModal.addEventListener('click', ()=> {
+  newPlaceModal.classList.toggle('modal_open');
+})
 
 const initialCards = [
   {
@@ -32,17 +44,35 @@ const initialCards = [
     name: 'Monument Valley',
     link: './images/monument.jpg'
   }
-]
+];
+
+const element = document.querySelector('.element-template').content.querySelector('.element');
+const elements = document.querySelector('.elements');
+
+initialCards.forEach(data => {
+  
+  const elementContent = element.cloneNode(true);
 
 
-function controlEditModal () {
-  formName.value = profileName.textContent;
-  formAbout.value = profileAbout.textContent;
+
+  const elementImage = elementContent.querySelector('.element__image');
+  const elementTitle = elementContent.querySelector('.element__name');
+  const elementLike = elementContent.querySelector('.element__like');
+  const elementDelete  = elementContent.querySelector('.element__delete');
+  
+  elementTitle.textContent = data.name;
+  elementImage.style.backgroundImage = `url(${data.link})`;
+  elements.prepend(elementContent);
+});
+
+function toggleModal (modal) {
   modal.classList.toggle('modal_open');
 }
 
-function closingModal () {
-  modal.classList.toggle('modal_open');
+function openEditModal () {
+  formName.value = profileName.textContent;
+  formAbout.value = profileAbout.textContent;
+  toggleModal(editProfileModal);
 }
 
 
@@ -50,10 +80,13 @@ function formSubmit (e) {
   e.preventDefault ();
   profileName.textContent = formName.value;
   profileAbout.textContent = formAbout.value;
-  modal.classList.toggle('modal_open');
+  editProfileModal.classList.toggle('modal_open');
 }
 
 
-modalEditButton.addEventListener('click', controlEditModal);
-closeModal.addEventListener('click', closingModal);
+profileEditButton.addEventListener('click', openEditModal);
+//close Edit Profile Window
+closeEditProfileModal.addEventListener('click', () =>  {
+  toggleModal(editProfileModal);
+
 form.addEventListener('submit', formSubmit);
