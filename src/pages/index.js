fc1669c455f47dfb69e653328d5551b8df23aabd
vakhitovalex <vakhitovalex@gmail.com>
2020-11-22 21:51:35 -0800

    format index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,77 +33,76 @@ const api = new Api({
   },
 });
 
-function renderForm (isLoading, modal) {
+function renderForm(isLoading, modal) {
   if (isLoading) {
-    modal.querySelector(".form__submit").textContent = 'Saving...';
-  }
-  else {
-    modal.querySelector('.form__submit').textContent = 'Save';
+    modal.querySelector(".form__submit").textContent = "Saving...";
+  } else {
+    modal.querySelector(".form__submit").textContent = "Save";
   }
 }
 
 const currentUser = new UserInfo();
-api.getUserInfo()
-.then(res => {
-  currentUser.setUserInfo(res.name,res.about,res.avatar);
+api.getUserInfo().then((res) => {
+  currentUser.setUserInfo(res.name, res.about, res.avatar);
 });
 
-
-
 const profileEdit = new PopupWithForm({
-  popupSelector: '.modal_type_edit-profile',
+  popupSelector: ".modal_type_edit-profile",
   handleFormSubmit: (formInputs) => {
     renderForm(true, editProfileModal);
-    api.updateUserInfo({name: formInputs.profileName, about: formInputs.profileAbout})
-    .then((res) => {
-    currentUser.setUserInfo(res.name,res.about, res.avatar);
-    })
-    .finally(() => {
-      renderForm(false, editProfileModal);
-      profileEdit.close();
-    });
-  }
+    api
+      .updateUserInfo({
+        name: formInputs.profileName,
+        about: formInputs.profileAbout,
+      })
+      .then((res) => {
+        currentUser.setUserInfo(res.name, res.about, res.avatar);
+      })
+      .finally(() => {
+        renderForm(false, editProfileModal);
+        profileEdit.close();
+      });
+  },
 });
 
 profileEdit.setEventListeners();
 
-profileEditButton.addEventListener('click', () => {
+profileEditButton.addEventListener("click", () => {
   const userValues = currentUser.getUserInfo();
   profileEdit.open();
   userProfileNameForm.value = userValues.name;
   userProfileAboutForm.value = userValues.job;
 });
 
-const profilePictureEdit = new PopupWithForm ({
-  popupSelector: '.modal_type_edit-profile-picture',
+const profilePictureEdit = new PopupWithForm({
+  popupSelector: ".modal_type_edit-profile-picture",
   handleFormSubmit: (formInputs) => {
     renderForm(true, editProfilePictureModal);
-    api.updateUserPicture(formInputs.pictureLink)
-    .then((res) => {
-      currentUser.setProfilePicture(res.avatar);
-      profilePictureEdit.close();
-    })
-    .catch((err) => {
-      console.log(err);
-      profilePictureEdit.close();
-    })
-    .finally(() => {
-      renderForm(false, editProfilePictureModal);
-      profilePictureEdit.close();
-    });
-  }
+    api
+      .updateUserPicture(formInputs.pictureLink)
+      .then((res) => {
+        currentUser.setProfilePicture(res.avatar);
+        profilePictureEdit.close();
+      })
+      .catch((err) => {
+        console.log(err);
+        profilePictureEdit.close();
+      })
+      .finally(() => {
+        renderForm(false, editProfilePictureModal);
+        profilePictureEdit.close();
+      });
+  },
 });
 profilePictureEdit.setEventListeners();
 
-profilePictureEditButton.addEventListener('click', () => {
+profilePictureEditButton.addEventListener("click", () => {
   profilePictureEdit.open();
 });
 
-api.getUserInfo()
-.then(res => {
+api.getUserInfo().then((res) => {
   const currentUserId = res._id;
-  api.getInitialCards()
-    .then((res) => {
+  api.getInitialCards().then((res) => {
     console.log(res);
     // console.log(currentUserId + " !!!")
     const initialElements = new Section(
@@ -127,28 +126,32 @@ api.getUserInfo()
                       deleteCardModal.close();
                     });
                     // console.log(cardData._id + " was deleted");
-                  }
+                  },
                 });
                 deleteCardModal.setEventListeners();
                 deleteCardModal.open();
-
               },
               handleLikeClick: (cardData) => {
-
                 // console.log(cardData.isLiked);
-                api.changeLikeStatus(cardData._id, cardData.isLiked)
-                .then(likesData => {
-                  // console.log(likesData)
-                  card.setLikesNumber(likesData.likes.length, cardData.isLiked);
-                  card.like();
-                }
-                  );
-
-              }
+                api
+                  .changeLikeStatus(cardData._id, cardData.isLiked)
+                  .then((likesData) => {
+                    // console.log(likesData)
+                    card.setLikesNumber(
+                      likesData.likes.length,
+                      cardData.isLiked
+                    );
+                    card.like();
+                  });
+              },
             },
             ".element-template"
           );
-          const cardElement = card.getCard(cardData.owner._id, currentUserId, cardData.likes);
+          const cardElement = card.getCard(
+            cardData.owner._id,
+            currentUserId,
+            cardData.likes
+          );
           initialElements.addItem(cardElement);
         },
       },
@@ -160,49 +163,59 @@ api.getUserInfo()
       popupSelector: ".modal_type_add-place",
       handleFormSubmit: (formInputs) => {
         renderForm(true, newPlaceModal);
-        api.addNewCard({ name: formInputs.placeTitle, link: formInputs.placeLink })
-        .then((res) => {
-          const card = new Card(
-            {
-              data: res,
-              currentUserId: currentUserId,
-              handleCardClick: (name, link) => {
-                photoModal.open(name, link);
-              },
-              handleDeleteClick: (cardData) => {
-                const deleteCardModal = new PopupWithSubmit({
-                  popupSelector: ".modal_type_delete-place",
-                  handleDeleteSubmit: () => {
-                    api.deleteCard(cardData._id).then(() => {
-                      card.removeCard();
-                      deleteCardModal.close();
+        api
+          .addNewCard({
+            name: formInputs.placeTitle,
+            link: formInputs.placeLink,
+          })
+          .then((res) => {
+            const card = new Card(
+              {
+                data: res,
+                currentUserId: currentUserId,
+                handleCardClick: (name, link) => {
+                  photoModal.open(name, link);
+                },
+                handleDeleteClick: (cardData) => {
+                  const deleteCardModal = new PopupWithSubmit({
+                    popupSelector: ".modal_type_delete-place",
+                    handleDeleteSubmit: () => {
+                      api.deleteCard(cardData._id).then(() => {
+                        card.removeCard();
+                        deleteCardModal.close();
+                      });
+                      // console.log(cardData._id + " was deleted");
+                    },
+                  });
+                  deleteCardModal.setEventListeners();
+                  deleteCardModal.open();
+                },
+                handleLikeClick: (cardData) => {
+                  api
+                    .changeLikeStatus(cardData._id, cardData.isLiked)
+                    .then((likesData) => {
+                      card.setLikesNumber(
+                        likesData.likes.length,
+                        cardData.isLiked
+                      );
+                      card.like();
                     });
-                    // console.log(cardData._id + " was deleted");
-                  }
-                });
-                deleteCardModal.setEventListeners();
-                deleteCardModal.open();
+                },
               },
-              handleLikeClick: (cardData) => {
-                api.changeLikeStatus(cardData._id, cardData.isLiked)
-                .then(likesData => {
-                  card.setLikesNumber(likesData.likes.length, cardData.isLiked);
-                  card.like();
-                }
-                  );
-
-              }
-            },
-            ".element-template"
-          );
-          const cardElement = card.getCard(res.owner._id, currentUserId, res.likes);
-          initialElements.addItem(cardElement);
-        })
-        .finally(()=> {
-          renderForm(false, newPlaceModal);
-          addNewCard.close();
-        });
-      }
+              ".element-template"
+            );
+            const cardElement = card.getCard(
+              res.owner._id,
+              currentUserId,
+              res.likes
+            );
+            initialElements.addItem(cardElement);
+          })
+          .finally(() => {
+            renderForm(false, newPlaceModal);
+            addNewCard.close();
+          });
+      },
     });
     addNewCard.setEventListeners();
 
@@ -211,7 +224,6 @@ api.getUserInfo()
     });
   });
 });
-
 
 const photoModal = new PopupWithImage(".modal_type_image");
 photoModal.setEventListeners();
@@ -229,7 +241,6 @@ const editProfilePictureModalValidator = new FormValidator(
   settings,
   profilePictureSubmitForm
 );
-
 
 editProfilePictureModalValidator.enableValidation();
 editProfileModalValidator.enableValidation();
